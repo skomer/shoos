b6ed9e17d1ac4b4b304af7091fac7e5fc088a969
Bertie Croll <bert.croll@gmail.com>
2016-10-18 14:33:14 +0100

    added database persistence

--- controller.rb
+++ controller.rb
@@ -9,5 +9,11 @@ end
 
 post '/shoos' do
   @order = Order.new(params)
+  @order.save()
   erb(:conf)
+end
+
+get '/shoos' do
+  @orders = Order.all()
+  erb(:index)
 end
--- models/order.rb
+++ models/order.rb
@@ -12,7 +12,20 @@ class Order
     @size = params['size'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO orders (customer_name, customer_address, quantity, size)
+      VALUES ('#{@customer_name}', '#{@customer_address}', #{@quantity}, #{@size})
+      RETURNING *"
+    result = SqlRunner.run(sql)
+    @id = result.first()['id'].to_i
+  end
 
+  def self.all()
+    sql = "SELECT * FROM orders"
+    result = SqlRunner.run(sql)
+    orders = result.map {|order| Order.new(order)}
+    return orders
+  end
 
 
 
